f8d50f52c00595cfa72a4da780d8be392952e013
Utilizador <Utilizador@192.168.30.178>
2018-02-07 11:18:50 +0000

    [add] test clean_fasta

---
+++ day_3/test_clea_fasta.py
@@ -0,0 +1,30 @@
+'''
+Created on 07/02/2018
+
+@author: Utilizador
+'''
+import unittest
+import second_module
+
+
+class Test(unittest.TestCase):
+
+
+    def test_clean_fasta(self):
+        self.assertEqual('AAACCCTTT', second_module.clean_fasta('aaacccttt'))
+        self.assertEqual('AAACCCTTT', second_module.clean_fasta('AAACCCTTT'))
+        self.assertEqual('AAACCCTTT', second_module.clean_fasta('sdfdvaaacccttt'))
+        self.assertEqual('', second_module.clean_fasta(''))
+        self.assertEqual('AAACCCTTT', second_module.clean_fasta('aaacccttt'))
+
+    def test_complement(self):
+        self.assertEqual('AATT', second_module.complement('TTAA'))
+        self.assertEqual('AATTGGCC', second_module.complement('TTAACCGG'))
+        
+    def test_reverse(self):
+        self.assertEqual('TTAA', second_module.reverse('TTA'))
+        
+        
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
